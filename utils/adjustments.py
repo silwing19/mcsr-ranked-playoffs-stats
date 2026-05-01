fd0d046b171a6f21008e194aa5ce62e583a37b04
silwing19 <silwing19@gmail.com>
2026-05-01 19:26:19 -0400

    Update adjustments.py

--- utils/adjustments.py
+++ utils/adjustments.py
@@ -1,5 +1,6 @@
 # puts placements on the same scale
-def rescale_placements(df):
+def rescale_placements(data):
+    df = data.copy()
     df["placement"] = (
                 df.groupby("season")["placement"]
                 .transform(lambda x: 1 + (x - x.min()) * 5 / (x.max() - x.min()))
@@ -9,6 +10,6 @@ def rescale_placements(df):
 def adjust(df, to_adjust, by, k = None):
     global_avg = df[to_adjust].mean()
     if k: k = k 
-    else: k = 3
+    else: k = df[to_adjust].median()
     df[to_adjust] = (df[to_adjust] * df[by] + global_avg * k) / (df[by] + k)
     return df.reset_index()
