1e2c10f4554433240286fc32e8e27ffd47e14a8e
silwing19 <silwing19@gmail.com>
2026-05-02 23:13:26 -0400

    Update sidebar.py

--- utils/sidebar.py
+++ utils/sidebar.py
@@ -1,4 +1,12 @@
 import streamlit as st
+from st_social_media_links import SocialMediaIcons
+
+social_media_links = [
+    "https://x.com/silwing19",
+    "https://github.com/silwing19",
+]
+
+social_media_icons = SocialMediaIcons(social_media_links)
 
 def make_sidebar():
     with st.sidebar:
@@ -39,4 +47,5 @@ def make_sidebar():
                 st.cache_data.clear()
         st.markdown('<span style="font-size:0.9em">sample sizes are small for a lot of players, so interpret everything with caution.</span>', unsafe_allow_html=True)
         st.markdown('<span style="font-size:0.9em">stats don\'t tell the whole story, so please be nice + don\'t take it too seriously!</span>', unsafe_allow_html=True)
+        social_media_icons.render()
         
