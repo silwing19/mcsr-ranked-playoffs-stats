c4e73a6a38b3a5e04cd4427caa6ee260ba6a5a32
silwing19 <silwing19@gmail.com>
2026-05-02 22:57:33 -0400

    Update sidebar.py

--- utils/sidebar.py
+++ utils/sidebar.py
@@ -30,7 +30,13 @@ def make_sidebar():
                 st.switch_page("pages/season.py")
             if st.button("matches"):
                 st.switch_page("pages/match.py")
+        st.markdown("<span style='font-size:0.9em'>i'll try to update this as matches happen (at least within the day, hopefully). if i've updated it and you still can't see the changes, try this button</span>", unsafe_allow_html=True)
+        with st.container(
+            horizontal=True,
+            horizontal_alignment='center'
+        ):
+            if st.button("refresh data"):
+                st.cache_data.clear()
         st.markdown('<span style="font-size:0.9em">sample sizes are small for a lot of players, so interpret everything with caution.</span>', unsafe_allow_html=True)
         st.markdown('<span style="font-size:0.9em">stats don\'t tell the whole story, so please be nice + don\'t take it too seriously!</span>', unsafe_allow_html=True)
-        if st.button("refresh data"):
-            st.cache_data.clear()
+        
